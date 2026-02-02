30157f1082eb23616b2c1f786d5410233d71b9f6
HannaButakova <iris.ld.hanna@gmail.com>
2026-02-02 15:37:47 +0200

    new 1-gallery.html

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -51,19 +51,18 @@ const images = [
 const galleryMarkup = images
   .map(
     ({ preview, original, description }) => `
-    <li class="gallery-item">
-      <a class="gallery-link" href="${original}">
-        <img class="gallery-image" src="${preview}" alt="${description}" />
-      </a>
-    </li>
-  `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img class="gallery-image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `
   )
   .join('');
 
 galleryEl.innerHTML = galleryMarkup;
 
 const lightbox = new SimpleLightbox('.gallery a', {
-  captions: true,
   captionsData: 'alt',
   captionDelay: 250,
 });
